851312ec5e8ba0e0ec49a58474048fe09bac6562
admin <admin@ubuntu20-portal-ng-tmpl.dsic.cloud>
2022-12-12 17:51:45 +0100

    Update for worker logic

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,41 +1,42 @@
-//const zmq = require('zeromq')
+const fs = require('fs')
+
 const path = require('path')
 const git = require('isomorphic-git')
 const http = require('isomorphic-git/http/node')
-const fs = require('fs')
 
-/*
-let req = zmq.socket('req')
-req.identity = 'Worker1'
-var args = process.argv.slice(2)
-if (args.length < 1) {
-  console.log ("node myclient brokerURL")
-  process.exit(-1)
-}
-var bkURL   = args[0]
-console.log(bkURL+"i'm worker")
-req.connect(bkURL)
-req.on('message', (c,sep,msg)=> {
-	console.log("received",c+'',msg+'')
-	setTimeout(()=> {
-		req.send([c,'','resp'])
-	}, 1000)
-})
-req.send(['','',''])
-*/
-async function workInJob(){ 
-	const dir = path.join(process.cwd(), 'workDir')
-	await git.clone({ 
-		fs, 
-		http, 
-		dir, 
-		url: 'https://github.com/isomorphic-git/lightning-fs' 
-	}).then(console.log('Git repository cloned correctly'))
+const systemDirection = path.join(process.cwd(), 'framework')
 
+function removeDirectory() {
 	console.log('Execute order 66')
-	fs.rmSync(dir, { recursive: true, force: true });
+	try {
+		fs.rmSync(systemDirection, { recursive: true, force: true })
+	} catch (error) {
+		console.log('The directory cannot be removed')
+	}
 }
 
-workInJob()
+async function workInJob(repository){ 
+	await clone(repository)
+	runJob()
+	removeDirectory(systemDirection)
+}
+
+async function clone(repository) {
+	await git.clone({
+		fs,
+		http,
+		dir: systemDirection,
+		url: repository
+	}).then(console.log('Git repository cloned correctly'))
+}
+
+function runJob(){
+	console.log('Running job')
+
+} 
+
+workInJob('https://github.com/isomorphic-git/lightning-fs')
+
+
 
 
