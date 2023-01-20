23cb5d2d40dc98f3bb0537019ce1f5b979226581
quentin-rgd <quentin.regaud31@gmail.com>
2023-01-20 01:21:21 +0100

    Consume message

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,43 +1,73 @@
-const fs = require('fs')
+setTimeout(function(){console.log("Hello worker!")}, 45000)
+setTimeout(function(){go()}, 45000)
 
-const path = require('path')
-const git = require('isomorphic-git')
-const http = require('isomorphic-git/http/node')
+function go(){
 
-const systemDirection = path.join(process.cwd(), 'framework')
+	const kafka = require('./kafka')   
+	const consumer = kafka.consumer({
+		groupId: "worker"
+	 })
 
-function removeDirectory() {
-	console.log('Execute order 66')
-	try {
-		fs.rmSync(systemDirection, { recursive: true, force: true })
-	} catch (error) {
-		console.log('The directory cannot be removed')
+	const fs = require('fs')
+
+	const path = require('path')
+	const git = require('isomorphic-git')
+	const http = require('isomorphic-git/http/node')
+
+	const systemDirection = path.join(process.cwd(), 'framework')
+
+	const main = async () => {
+		await consumer.connect()
+		await consumer.subscribe({
+			topic: "test-projet",
+		   	fromBeginning: true
+		})
+
+		await consumer.run({
+			eachMessage: async ({ topic, partition, message }) => {
+			   console.log('Received message', {
+				  topic,
+				  partition,
+				  key: message.key.toString(),
+				  value: message.value.toString()
+			   })
+			}
+		}) 
+	}	
+	main();
+
+	function removeDirectory() {
+		console.log('Execute order 66')
+		try {
+			fs.rmSync(systemDirection, { recursive: true, force: true })
+		} catch (error) {
+			console.log('The directory cannot be removed')
+		}
 	}
+
+	async function workInJob(repository){ 
+		await clone(repository)
+		runJob()
+		removeDirectory(systemDirection)
+	}
+
+	async function clone(repository) {
+		await git.clone({
+			fs,
+			http,
+			dir: systemDirection,
+			url: repository
+		}).then(console.log('Git repository cloned correctly'))
+	}
+
+	function runJob(){
+		console.log('Running job')
+
+	} 
+
+	workInJob('https://github.com/isomorphic-git/lightning-fs')
+
+	module.exports = {workInJob} 
 }
-
-async function workInJob(repository){ 
-	await clone(repository)
-	runJob()
-	removeDirectory(systemDirection)
-}
-
-async function clone(repository) {
-	await git.clone({
-		fs,
-		http,
-		dir: systemDirection,
-		url: repository
-	}).then(console.log('Git repository cloned correctly'))
-}
-
-function runJob(){
-	console.log('Running job')
-
-} 
-
-workInJob('https://github.com/isomorphic-git/lightning-fs')
-
-module.exports = {workInJob} 
-
 
 
