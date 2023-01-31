e11fcf88f45ab596fc024f2a0e019023323f2f42
albaiher <albaiher99@gmail.com>
2023-01-31 22:05:37 +0100

    Minor fix to script

--- src/exec.sh
+++ src/exec.sh
@@ -1,8 +1,7 @@
 #Build images
-docker build -t kafkajs ./configuration/
+docker build -t kafkajs ./base/
 docker build -t broker ./broker/
-docker build -t client ./client/
-docker build -t entry ./entry/
+docker build -t frontend ./frontend/
 docker build -t worker ./worker/
 
 echo -e "\n\n******\nImagenes creadas\n******\n\n"
